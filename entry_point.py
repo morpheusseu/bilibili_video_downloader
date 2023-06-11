8e235cfb15280f15a99309d2165e66f0ca0076f9
morpheusseu <morpheus_seu@iclou.com>
2023-06-11 15:11:03 +0800

    feature: enable extract fid or bvid from web link to download

--- entry_point.py
+++ entry_point.py
@@ -66,6 +66,34 @@ def download_video_from_given_bvid(convert_type, bv_id, credential, page_idx=0, 
     progress.stop() if progress else None
 
 
+def download_videos_from_given_link_adjustment(web_link, convert_type, credential, progress=None):
+    progress = Progress(
+        SpinnerColumn(), '{task.description}', BarColumn(), MofNCompleteColumn(
+        ), TaskProgressColumn(), TimeElapsedColumn(), TimeRemainingColumn()
+    ) if progress else None
+    parts = web_link.split('/')
+    video_flag = False
+    for item in parts:
+        if item.startswith("http"):
+            pass
+        if item.startswith("favlist?fid="):
+            fid = item.split('=')[1].split('&')[0]
+            # get favlist id
+            download_videos_from_given_favorite_list_id(
+                credential=credential, fid=fid, convert_type=convert_type, progress=progress)
+            break
+        if video_flag:
+            if item.startswith('BV'):
+                bvid = item
+                # get BV num
+                download_video_from_given_bvid(
+                    convert_type=convert_type, bv_id=bvid, credential=credential, progress=progress)
+            break
+        if item == 'video':
+            video_flag = True
+    progress.stop() if progress else None
+
+
 def hello(a=0, b=1):
     print("entry point {}|{}".format(a, b))
 
